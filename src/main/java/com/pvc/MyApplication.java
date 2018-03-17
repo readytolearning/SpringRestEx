d1790259937a7c0c0aa29f389752e83f08cfe3e8
readytolearning <polneedi.venkatesh@gmail.com>
2018-03-17 19:21:49 +0530

    changed files

--- src/main/java/com/pvc/MyApplication.java
+++ src/main/java/com/pvc/MyApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author P.Venkatesh
+ * @author P.Venkatesh 
  *
  */
 @SpringBootApplication
 public class MyApplication {
 	public static void main(String[] args) {
+		
 		SpringApplication.run(MyApplication.class, args);
+		
 	}
 }
