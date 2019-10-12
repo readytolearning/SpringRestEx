e4196e8d8c47fb2db921ef206f49deeba0379732
Venkatesh <polneedi.venkatesh@gmail.com>
2019-10-12 21:33:37 +0530

    CrossOrigin changes

--- src/main/java/com/pvc/resource/UserController.java
+++ src/main/java/com/pvc/resource/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("rest")
 public class UserController {
@@ -20,6 +23,16 @@ public class UserController {
 	public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
 		User user = userService.getUserById(id);
 		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+
+	@GetMapping("users")
+	@CrossOrigin(origins = "http://localhost:4200")
+	public ResponseEntity<List<User>> getUsers() {
+		List<User> users = new ArrayList<>();
+		User user = new User();
+		user.setName("Venkat");
+		users.add(user);
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
 	}
 	
 	@PostMapping("user")
