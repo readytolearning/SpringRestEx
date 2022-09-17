91d192f3755c51f2e80a805e3f97f5d4587321e7
readytolearning <polneedi.venkatesh@gmail.com>
2022-09-17 18:46:02 +0530

    Code change to test

--- src/main/java/com/pvc/MyApplication.java
+++ src/main/java/com/pvc/MyApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyApplication {
 	public static void main(String[] args) {
+		
 		SpringApplication.run(MyApplication.class, args);
 	}
 }
